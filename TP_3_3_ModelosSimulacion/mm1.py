13a2e7f6eb0bc65002e8d04030ddcfeabba8937c
Biscaldi-Iv <ivanbisc12@gmail.com>
2023-08-01 00:52:25 -0300

    partial mm1

--- TP_3_3_ModelosSimulacion/mm1.py
+++ TP_3_3_ModelosSimulacion/mm1.py
@@ -21,14 +21,28 @@ class MM1:
         self.inQat = []
         self.serverIsFree = True
 
+    def calc(self):
+        self.meanTimeInQ = 0
+        self.meanTimeInSys = 0
+
+    def graphs(self):
+        fig, ax = plt.subplots()
+        ax.set_title('Clientes en cola')
+        ax.set(xlabel='Tiempo', ylabel='Cant. Clientes')
+        xs = [p[0] for p in self.inQat]
+        ys = [p[1] for p in self.inQat]
+        ax.plot(xs, ys)
+        plt.show()
+
     def Simulate(self, maxTime: int):
+        self.inQat.append((0, 0))
         # First arrival
         self.time = self.arrivalTimes.pop(0)
         self.events.append("arrival")
         self.eventTimes.append(self.time)
         self.serverIsFree = False  # direct to server
         # self.inQueue += 1 #given user went direct to server, queue didnt increase
-        self.inQat.append((self.inQueue, self.time))
+        self.inQat.append((self.time, self.inQueue))
 
         while(self.time < maxTime and len(self.arrivalTimes) > 0 and len(self.serveTimes) > 0):
             if(self.serveTimes[0] < self.arrivalTimes[0] and not self.serverIsFree):
@@ -51,15 +65,17 @@ class MM1:
                     continue
                 else:
                     self.events.append("arrival")
-                    if not self.serverIsFree:  # server is not free
+                    if not self.serverIsFree:  # server is not freed
                         self.inQueue += 1  # new user gets into queue
-                    else:  # server is free
+                    else:  # server is freed
                         self.serverIsFree = False  # new user gets direct to server
-            self.inQat.append((self.inQueue, self.time))
+            self.inQat.append((self.time, self.inQueue))
+
+        self.graphs()
 
 
 m = MM1(1, 1.25, 5)
 m.Simulate(200)
-print(m.events)
-print(m.inQat)
+print(m.events, end='\r\n\r\n')
+print(m.inQat, end='\r\n\r\n')
 print(f"{m.rejected=}, {m.served=}")
